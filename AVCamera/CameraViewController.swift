c53dae2ab129fb4d1debd5cc0335c2b049156bd6
Adaicon <adaicon@192.168.0.101>
2025-02-16 23:07:24 +0800

    first commit

---
+++ AVCamera/CameraViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CameraViewController.swift
+//  AVCamera
+//
+//  Created by Adaicon on 2025/2/16.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+    var cameraView: UIView?
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = UIColor.white
+        setupCameraView()
+    }
+    
+    func setupCameraView() {
+        let width = self.view.frame.width
+        let height = width * 16.0 / 9.0
+        
+        cameraView = UIView(frame: CGRect(x: 0, y: 44, width: width, height: height))
+        cameraView?.backgroundColor = UIColor.black
+        
+        if let view = cameraView {
+            self.view.addSubview(view)
+        }
+    }
+}
+
